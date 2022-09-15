2cfa576f512bb72d686f8d95f0f23c1268f647c5
abu bokor suraj <abusurajbd@gmail.com>
2022-09-15 12:22:02 +0600

    text-interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'v-series';
+export class AppComponent{
+  title = "Abu Suraj";
+  roll = 0;
+  getMax(a:number, b: number, c: number){
+   if(a>b){
+    if(a>c){return a;}
+    else return c;
+   }
+   else{
+    if(b>c) return b;
+    else return c;
+   }
+  }
 }
+// export class AppComponent implements OnInit {
+//   // title = 'v-series';
+//   intervalSub: any ;
+//   ngOnInit(){
+//     this.intervalSub = setInterval(() =>{
+//       console.log("hello from ngOninit")
+//     },1000);
+//   }
+//   ngOnDestroy(){
+//     if(this.intervalSub){
+//       clearInterval(this.intervalSub)
+//     }
+//   }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HelloworldComponent} from './hello-world.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HelloworldComponent
   ],
   imports: [
     BrowserModule,
