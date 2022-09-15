ce210f605c083e3265079a83bff9766e56600532
abu bokor suraj <abusurajbd@gmail.com>
2022-09-15 14:18:30 +0600

    child to parent @input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HelloworldComponent} from './hello-world.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+
 @NgModule({
   declarations: [
-    AppComponent, HelloworldComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
